b2a779cbff86a9612325c7f48970350e6e01d840
amos-ai <60488908+amos-ai@users.noreply.github.com>
2026-07-21 18:15:48 +0200

    completeed variables excercises

--- src/main/java/com/amigoscode/_1_beginners/_1_Variables.java
+++ src/main/java/com/amigoscode/_1_beginners/_1_Variables.java
@@ -8,30 +8,37 @@ package com.amigoscode._1_beginners;
  */
 public class _1_Variables {
 
-    public static void main(String[] args) {
+    static void main() {
 
         // TODO: 1 - Declare an int variable called age and assign it the value 25
-
+        int age = 25;
 
         // TODO: 2 - Declare a double variable called price and assign it the value 9.99
-
+        double price = 9.99;
 
         // TODO: 3 - Declare a boolean variable called isJavaFun and assign it the value true
-
+        boolean isJavaFun = true;
 
         // TODO: 4 - Declare a String variable called name and assign it your name
-
+        String name = "Amos";
 
         // TODO: 5 - Declare a char variable called grade and assign it the value 'A'
-
+        char grade = 'A';
 
         // TODO: 6 - Print all the variables above using System.out.println
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("isJavaFun: " + isJavaFun);
+        System.out.println("Name: " + name);
+        System.out.println("Grade: " + grade);
         // Hint: You can print each variable on its own line, e.g.:
         //   System.out.println("Age: " + age);
 
 
         // TODO: 7 - Declare a final (constant) variable called MAX_SCORE, set it to 100, and print it
         // Hint: Use the 'final' keyword before the type to make a constant
+         final int MAX_SCORE = 100;
+        System.out.println("MAX_SCORE: " + MAX_SCORE);
 
     }
 }
